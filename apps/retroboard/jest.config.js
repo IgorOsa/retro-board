1567c0a31cb34aeb7377597bc3ce36d5f4904e5d
Ihor Osadchyi <igorosa.com@gmail.com>
2021-11-15 18:30:45 +0200

    initial commit

---
+++ apps/retroboard/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'retroboard',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/retroboard',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
