61b8a183bffd19bdfbe375dc1c4a2921ee413a92
Ihor Osadchyi <igorosa.com@gmail.com>
2021-11-16 15:14:14 +0200

    feat: add simple footer

--- apps/retroboard/src/app/shared/shared.module.ts
+++ apps/retroboard/src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule, MaterialModule],
-  exports: [HeaderComponent, MaterialModule],
+  exports: [FooterComponent, HeaderComponent, MaterialModule],
 })
 export class SharedModule {}
