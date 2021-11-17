36a2411cf22aab9e72c4595152dd4d7a2fab288f
Ihor Osadchyi <igorosa.com@gmail.com>
2021-11-17 21:33:48 +0200

    feat: add loader component

--- apps/retroboard/src/app/shared/shared.module.ts
+++ apps/retroboard/src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MaterialModule } from './material.module';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, LoaderComponent],
   imports: [CommonModule, MaterialModule],
-  exports: [FooterComponent, HeaderComponent, MaterialModule],
+  exports: [FooterComponent, HeaderComponent, MaterialModule, LoaderComponent],
 })
 export class SharedModule {}
