6236902ae409c45a6fcbc99db70b498fac31acf5
Ihor Osadchyi <igorosa.com@gmail.com>
2021-11-18 16:49:15 +0200

    feat: add db module for connecting to mongo

--- apps/api/src/app/board/board.module.ts
+++ apps/api/src/app/board/board.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { DbModule } from '../db/db.module';
 import { BoardController } from './board.controller';
+import { boardProviders } from './board.providers';
 import { BoardService } from './board.service';
 
 @Module({
+  imports: [DbModule],
   exports: [BoardModule],
   controllers: [BoardController],
-  providers: [BoardService],
+  providers: [BoardService, ...boardProviders],
 })
 export class BoardModule {}
---
+++ apps/api/src/app/board/board.providers.ts
@@ -0,0 +1,10 @@
+import { Mongoose } from 'mongoose';
+import { BoardSchema } from './schemas/board.schema';
+
+export const boardProviders = [
+  {
+    provide: 'BOARD_MODEL',
+    useFactory: (mongoose: Mongoose) => mongoose.model('Board', BoardSchema),
+    inject: ['DATABASE_CONNECTION'],
+  },
+];
