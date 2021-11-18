8902b08032d1364eeb49d7644814e47f157c41eb
Ihor Osadchyi <igorosa.com@gmail.com>
2021-11-18 16:48:46 +0200

    feat: add dotenv

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -1,3 +1,10 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({
+  path: path.join(__dirname, './../../.env'),
+});
+
 export const environment = {
   production: false,
 };
