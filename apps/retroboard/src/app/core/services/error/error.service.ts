5cc87b138274bc65a7191218aeddc24fb3aae2a1
Ihor Osadchyi <igorosa.com@gmail.com>
2021-11-27 14:04:51 +0200

    fix: logout if jwt expired

--- apps/retroboard/src/app/core/services/error/error.service.ts
+++ apps/retroboard/src/app/core/services/error/error.service.ts
@@ -1,18 +1,32 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { throwError } from 'rxjs';
 import { SnackbarService } from '../../../shared/services/snackbar/snackbar.service';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ErrorService {
-  constructor(private readonly snackBarService: SnackbarService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private readonly snackBarService: SnackbarService
+  ) {}
 
   public handleError$(err: HttpErrorResponse) {
-    const msg =
+    let msg =
       `${err.error.statusCode} ${err.error.message}`.trim() || 'Unknown Error';
+
+    if (err.status === 401) {
+      msg = 'Auth session expired. Please, login again';
+      this.authService.logout();
+      this.router.navigateByUrl('/login');
+    }
+
     this.snackBarService.open(msg, '');
+
     return throwError(() => new Error(err.message));
   }
 }
--- apps/retroboard/src/app/modules/user/services/users.service.ts
+++ apps/retroboard/src/app/modules/user/services/users.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IUserResponse } from '@tutorio/api-interfaces';
+import { IUserResponse } from '@retro-board/api-interfaces';
 
 @Injectable({
   providedIn: 'root',
