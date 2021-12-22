e21c04b13d275ccc1a3a9ef7d1ceeb400400205f
Ihor Osadchyi <igorosa.com@gmail.com>
2021-12-22 16:09:07 +0200

    feat: canLoad for LazyModules

--- apps/retroboard/src/app/modules/board/board-routing.module.ts
+++ apps/retroboard/src/app/modules/board/board-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from '../../core/guards/auth.guard';
 import { BoardComponent } from './components/board/board.component';
 
 const routes: Routes = [
@@ -8,7 +7,6 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: BoardComponent,
-    canActivate: [AuthGuard],
   },
 ];
 
