1fea7af136f836b0cc14e699cfa9c0a6979501eb
Ihor Osadchyi <igorosa.com@gmail.com>
2021-12-23 19:07:56 +0200

    feat: content loading component

--- apps/retroboard/src/app/shared/shared.module.ts
+++ apps/retroboard/src/app/shared/shared.module.ts
@@ -1,19 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 import { MaterialModule } from './material.module';
 import { LoaderComponent } from './components/loader/loader.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxContentLoadingModule } from 'ngx-content-loading';
+
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 import { ValidatorPipe } from './pipes/validator/validator.pipe';
+import { ContentLoadingComponent } from './components/content-loading/content-loading.component';
 
 @NgModule({
-  declarations: [ConfirmDialogComponent, LoaderComponent, ValidatorPipe],
-  imports: [CommonModule, FormsModule, MaterialModule, ReactiveFormsModule],
-  exports: [
+  declarations: [
+    ContentLoadingComponent,
+    ConfirmDialogComponent,
+    LoaderComponent,
+    ValidatorPipe,
+  ],
+  imports: [
+    CommonModule,
     FormsModule,
     MaterialModule,
+    NgxContentLoadingModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    ContentLoadingComponent,
+    FormsModule,
     LoaderComponent,
+    MaterialModule,
+    NgxContentLoadingModule,
     ReactiveFormsModule,
     ValidatorPipe,
   ],
